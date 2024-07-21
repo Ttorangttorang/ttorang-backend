82d5b8554e057cc6c5842182624d21aa9f707f1d
zzjiho <youngyoungnim@gmail.com>
2024-07-22 00:39:41 +0900

    init

---
+++ src/main/java/com/ttorang/TtorangApplication.java
@@ -0,0 +1,13 @@
+package com.ttorang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtorangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtorangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttorang/TtorangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttorang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtorangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
