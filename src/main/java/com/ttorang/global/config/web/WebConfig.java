a64ffe0a3742b4c5e63257fdabda3714bb6a0c76
zzjiho <youngyoungnim@gmail.com>
2024-07-27 14:27:58 +0900

    fix : addMapping

--- src/main/java/com/ttorang/global/config/web/WebConfig.java
+++ src/main/java/com/ttorang/global/config/web/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("*")
+        registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000", "http://ttorang.site")
                 .allowedHeaders("*")
                 .allowedMethods(
