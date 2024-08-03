17f1dad52c9888a5ab0695ad4fa2d3fd60fa4f7b
zzjiho <youngyoungnim@gmail.com>
2024-08-04 00:59:30 +0900

    feat: 카카오 로그인 api

--- src/main/java/com/ttorang/web/kakaotoken/controller/KakaoTokenController.java
+++ src/main/java/com/ttorang/web/kakaotoken/controller/KakaoTokenController.java
@@ -2,9 +2,12 @@ package com.ttorang.web.kakaotoken.controller;
 
 import com.ttorang.global.error.exception.Custom404Exception;
 import com.ttorang.global.util.EnvironmentUtil;
+import com.ttorang.global.util.RequestUrlUtil;
 import com.ttorang.web.kakaotoken.client.KakaoTokenClient;
 import com.ttorang.web.kakaotoken.dto.KakaoTokenDto;
 import com.ttorang.web.kakaotoken.util.KakaoApiUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +15,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@Tag(name = "kakao", description = "카카오 로그인 API")
 public class KakaoTokenController {
 
     private final EnvironmentUtil env;
@@ -35,23 +42,25 @@ public class KakaoTokenController {
         return "kakaoLoginForm";
     }
 
-    //Step 1: 인가 코드 받기
+    @Operation(
+            summary = "카카오 로그인 API ",
+            description = "인가 코드를 받은 후 카카오 토큰을 받습니다."
+    )
+    // step1 : 인가코드 받기
     @GetMapping("/kakao")
     public void kakaoLogin(
             HttpServletRequest request,
-            HttpServletResponse response
-    ) {
+            HttpServletResponse response) {
 
         String clientId = env.getKakaoClientId();
 
-        // 결과를 처리할 URI 정보
-//        String redirectUri = "http://localhost:8080/oauth/kakao/callback";
-        String redirectUri = "https://ttorang.site/oauth/kakao/callback";
+        String serviceUri = RequestUrlUtil.getServiceURL(request);
+        String redirectUri = "/oauth/kakao/callback";
+        String resultUri = serviceUri + redirectUri;
+        log.info("[-] kakaoLogin resultUri : {}", resultUri);
 
         // 카카오 accessToken 발급 요청
-        // 해당 서비스 URL이 유효한 URL인지 확인
-        String kakaoCallURL = KakaoApiUtil.getKakaoAuthorizeURL(clientId, redirectUri);
-
+        String kakaoCallURL = KakaoApiUtil.getKakaoAuthorizeURL(clientId, resultUri);
         log.info("[-] kakaoLogin Kakao First CallURL : {}", kakaoCallURL);
 
         try {
@@ -62,16 +71,18 @@ public class KakaoTokenController {
         }
     }
 
-    //Step 2 : 토큰받기
+    // step2 : 인가코드로 카카오 토큰 받기
     @GetMapping("/oauth/kakao/callback")
-    public ResponseEntity<KakaoTokenDto.Response> loginCallback(String code) {
+    public ResponseEntity<KakaoTokenDto.Response> loginCallback(
+            @RequestParam("code") String code) {
+
         String contentType = "application/x-www-form-urlencoded;charset=utf-8";
         KakaoTokenDto.Request kakaoTokenRequestDto = KakaoTokenDto.Request.builder()
                 .client_id(clientId)
                 .client_secret(clientSecret)
                 .grant_type("authorization_code")
                 .code(code)
-                .redirect_uri("https://ttorang.site/oauth/kakao/callback")
+                .redirect_uri("https:/ttorang.site//oauth/kakao/callback")
                 .build();
 
         log.info("code : {}", code);
